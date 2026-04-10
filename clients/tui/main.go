e6d1805f047ad770a2872bc4123ae25f5c034c32
Reid Mason <maddogshain132@gmail.com>
2026-04-10 02:04:30 +0100

    feat: removed event loop requirement from tui

--- clients/tui/main.go
+++ clients/tui/main.go
@@ -17,16 +17,15 @@ import (
 
 type model struct {
 	text     string
-	pomodoro *models.Pomodoro
+	pomodoro models.Pomodoro
 }
 
-func startWsClient(pom *models.Pomodoro) {
+func startWsClient(program *tea.Program) {
 	interrupt := make(chan os.Signal, 1)
 	signal.Notify(interrupt, os.Interrupt)
 
 	addr := "localhost:8080"
 	u := url.URL{Scheme: "ws", Host: addr, Path: "/ws"}
-	// log.Printf("connecting to %s", u.String())
 
 	c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
 	if err != nil {
@@ -44,7 +43,12 @@ func startWsClient(pom *models.Pomodoro) {
 				log.Println("read:", err)
 				return
 			}
-			json.Unmarshal(message, pom)
+			var pom models.Pomodoro
+			err = json.Unmarshal(message, &pom)
+			if err != nil {
+				log.Println("Failed to unmarshal pomodoro data", err)
+			}
+			program.Send(newPomodoroData(pom))
 		}
 	}()
 
@@ -79,16 +83,16 @@ func startWsClient(pom *models.Pomodoro) {
 
 func main() {
 	pom := models.Pomodoro{}
+	m := initModel(pom)
 
-	go startWsClient(&pom)
-	m := initModel(&pom)
 	p := tea.NewProgram(m)
+	go startWsClient(p)
 	if _, err := p.Run(); err != nil {
 		log.Fatalf("Error starting program: %v", err)
 	}
 }
 
-func initModel(pomodoro *models.Pomodoro) model {
+func initModel(pomodoro models.Pomodoro) model {
 	return model{
 		text:     "Connecting...",
 		pomodoro: pomodoro,
@@ -102,16 +106,20 @@ func (m model) Init() tea.Cmd {
 type tickMsg time.Time
 
 func scheduleTick() tea.Cmd {
-	return tea.Every(time.Millisecond*100, func(t time.Time) tea.Msg {
+	return tea.Every(time.Second, func(t time.Time) tea.Msg {
 		return tickMsg(t)
 	})
 }
+
+type newPomodoroData models.Pomodoro
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 	case tickMsg:
 		m.text = time.Time(msg).Format("01/02 03:04:05PM")
 		return m, scheduleTick()
+	case newPomodoroData:
+		m.pomodoro = models.Pomodoro(msg)
 	case tea.KeyPressMsg:
 		switch msg.String() {
 		case "ctrl+c", "q":
